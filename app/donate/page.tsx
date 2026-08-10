35e3dbe05eda2d75bda30c33386906d0a3a9a4a8
Amor Marketing <hello@amormarketingla.com>
2026-08-10 16:29:10 -0700

    Initial commit

---
+++ app/donate/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from "globals.css";
+
+
+
+export default function DonatePage() {
+    return (
+        <main>
+            <div className="section-heading">
+                <h2>Donate</h2>
+            </div>
+
+            <div className="contact__intro">
+                <p>Give us money.</p>
+            </div>
+        </main>
+    );
+}
